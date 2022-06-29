ae8e5fe4661afbc2893f983d1927ff968379b3c8
Zekarias Getaneh <archisofttechnology.com>
2022-06-29 23:28:40 +0100

    correction

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print("{:s}".format(str * 3))
-print("{:s}".format(str[:9]))
+print("{}{}{}".format(str, str, str))
+str = str[0:9]
+print("{}".format(str))
