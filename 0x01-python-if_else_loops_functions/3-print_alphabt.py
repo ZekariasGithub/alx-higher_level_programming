50992db0a7ff1ba1db845625754bd067525a5b80
Zekarias Getaneh <archisofttechnology.com>
2022-06-29 23:00:50 +0100

    correction

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for abc in range(97, 123):
-    if abc != 101 and abc != 113:
-        print("{:c}".format(abc), end='')
+# Author - Tolulope Fakunle
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
