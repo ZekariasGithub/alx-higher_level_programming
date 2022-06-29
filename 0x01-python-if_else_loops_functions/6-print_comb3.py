22c954d5b8098522ff737ff63c4731829c051116
Zekarias Getaneh <archisofttechnology.com>
2022-06-29 22:58:44 +0100

    correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for j in range(0, 10):
-    for k in range(j + 1, 10):
-        if j == 8 and k == 9:
-            print("89")
-        elif j != k:
-            print("{:d}{:d}".format(j, k), end=", ")
+# Author - Tolulope Fakunle
+
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
